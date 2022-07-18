b00f1a2d6a6ecdbc65dca7c53c6ea9be736035c6
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-18 22:09:06 +0300

    add map array method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import './index.css'
+import { Users } from './User';
+import { useState } from 'react';
 
 function App() {
+  const [query, setQuery] = useState("");
+  console.log(query)
   return (
-    <>
-    App
-    </>
+    <div className="app">
+      <input type="text"
+         placeholder="search..." 
+         className="search" 
+         onChange={(e) => setQuery(e.target.value)}
+       />
+      <ul className="list">
+{Users.map((user) =>(
+    <li key ={user.id}className="listItem">{user.first_name}</li>
+))}
+        
+      </ul>
+    </div>
   );
 }
 
